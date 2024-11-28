dd1fc5ca6662e8ab5afcf01a03cf2a1165c52387
SvitlanaTurych bc2024-6 <svitlana.study24@gmail.com>
2024-11-28 18:15:22 +0200

    part 2,3

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,12 @@
-FROM node
-WORKDIR /app
-COPY package.json ./
-RUN npm install
+ARG NODE_VERSION=23.1.0
+FROM node:${NODE_VERSION}-alpine
+ENV NODE_ENV production
+WORKDIR /usr/src/app
+RUN --mount=type=bind,source=package.json,target=package.json \
+    --mount=type=bind,source=package-lock.json,target=package-lock.json \
+    --mount=type=cache,target=/root/.npm \
+    npm ci
+USER node
 COPY . .
-CMD ["npx", "nodemon", "index.js", "--host", "0.0.0.0", "--port", "8080", "--cache", "notes"]
+EXPOSE 3000
+CMD npx nodemon -L --inspect=0.0.0.0:9229 index.js --host 0.0.0.0 -p 3000 -c notes
--- index.js
+++ index.js
@@ -5,6 +5,19 @@ const { Command } = require('commander');
 const program = new Command();
 const multer = require ('multer');
 const mmmm = multer()
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+const swaggerSpec = swaggerJsdoc({
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "API Documentation",
+      version: "1.0.0",
+    },
+  },
+  apis: [__filename], 
+});
+
 program
   .requiredOption('-h, --host <host>', 'server host')
   .requiredOption('-p, --port <port>', 'server port')
@@ -14,8 +27,31 @@ program.parse(process.argv);
 const options = program.opts();
 
 const app = express();
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use(express.json());
 
+/**
+ * @swagger
+ * /notes/{note_name}:
+ *   get:
+ *     summary: Отримати нотатку за іменем файлу
+ *     parameters:
+ *       - in: path
+ *         name:  note_name
+ *         required: true
+ *         description: Назва файлу(нотатки)
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Нотатку успішно отримано
+ *         content:
+ *           text/plain:
+ *             schema:
+ *               type: string
+ *       404:
+ *         description: Нотатку не знайдено
+ */
 
 app.get('/notes/:name', (req, res) => {
   const notePath = path.join(options.cache, `${req.params.name}.txt`);
@@ -26,6 +62,35 @@ app.get('/notes/:name', (req, res) => {
   res.send(noteText);
 });
 
+/**
+ * @swagger
+ * /notes/{note_name}:
+ *   put:
+ *     summary: Оновити існуючу нотатку за іменем файлу
+ *     parameters:
+ *       - in: path
+ *         name: note_name
+ *         required: true
+ *         description: Назва файлу(нотатки)
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       description: Текст для оновлення нотатки
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               text:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Нотатку успішно оновлено
+ *       404:
+ *         description: Нотатку не знайдено
+ */
+
 app.put('/notes/:name', (req, res) => {
   const notePath = path.join(options.cache, `${req.params.name}.txt`);
   if (!fs.existsSync(notePath)) {
@@ -34,6 +99,25 @@ app.put('/notes/:name', (req, res) => {
   fs.writeFileSync(notePath, req.body.text);
   res.sendStatus(200);
 });
+
+/**
+ * @swagger
+ * /notes/{note_name}:
+ *   delete:
+ *     summary: Видалити нотатку за іменем файлу
+ *     parameters:
+ *       - in: path
+ *         name: name
+ *         required: true
+ *         description: Назва файлу(нотатки)
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Нотатку успішно видалено
+ *       404:
+ *         description: Нотатку не знайдено
+ */
 
 app.delete('/notes/:name', (req, res) => {
   const notePath = path.join(options.cache, `${req.params.name}.txt`);
@@ -44,6 +128,27 @@ app.delete('/notes/:name', (req, res) => {
   res.sendStatus(200);
 });
 
+/**
+ * @swagger
+ * /notes:
+ *   get:
+ *     summary: Отримати всі нотатки
+ *     responses:
+ *       200:
+ *         description: Успішно отримано список нотаток
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   name:
+ *                     type: string
+ *                   text:
+ *                     type: string
+ */
+
 app.get('/notes', (req, res) => {
   const files = fs.readdirSync(options.cache);
   const notes = files.map(file => {
@@ -53,6 +158,30 @@ app.get('/notes', (req, res) => {
   });
   res.status(200).json(notes);
 });
+
+/**
+ * @swagger
+ * /write:
+ *   post:
+ *     summary: Створити нову нотатку
+ *     requestBody:
+ *       required: true
+ *       description: Дані для створення нотатки
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               note_name:
+ *                 type: string
+ *               note:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Успішно створено нотатку
+ *       400:
+ *         description: Нотатка з такою назвою вже існує
+ */
 
 app.post('/write', mmmm.none(), (req, res) => {
   const noteName = req.body.note_name;
@@ -66,11 +195,21 @@ app.post('/write', mmmm.none(), (req, res) => {
   res.sendStatus(201);
 });
 
+/**
+ * @swagger
+ * /UploadForm.html:
+ *   get:
+ *     summary: Отримати форму для завантаження нотаток
+ *     responses:
+ *       200:
+ *         description: Успішно отримано форму
+ */
+
 app.get('/UploadForm.html', (req, res) => {
   const formPath = path.join(__dirname, 'UploadForm.html');
   res.sendFile(formPath);
 });
 
 app.listen(options.port, options.host, () => {
-  console.log(`Server running at http://${options.host}:${options.port}/`);
+  console.log(`Server runnig at http://${options.host}:${options.port}/`);
 });
